60c3100579b1cc3e41b4804a16fc3648f785d5d8
0badran <badran7299@yahoo.com>
2025-03-28 15:24:56 +0200

    Fix deploy issues

--- src/app/todo/completed/page.tsx
+++ src/app/todo/completed/page.tsx
@@ -1,15 +1,26 @@
 import RemoveTaskButton from "@/components/remove-task-button";
 import RestoreTaskButton from "@/components/restore-task-button";
-import { sql } from "@vercel/postgres";
+import ApiResponse from "@/helpers/api-response";
+import fetchHelper from "@/helpers/fetch-helper";
 import Image from "next/image";
 
 
 export default async function Completed() {
-    const { rows } = await sql`SELECT id, title FROM todos WHERE type = 'completed';`;
-    if (!rows.length) return <Image className="mx-auto" unoptimized src="/images/complete-empty.gif" alt="complete task empty" width={450} height={300} />
+    const res = await fetchHelper({
+        url: '/todos/get-completed-tasks',
+        method: 'get',
+    });
+    if (!res.ok) {
+        return
+    }
+    const data: ApiResponse = await res.json();
+    const courses: Array<{ id: number, title: string }> = data.data as [];
+
+    if (!courses.length) return <Image className="mx-auto" unoptimized src="/images/complete-empty.gif" alt="complete task empty" width={450} height={300} />
+
     return (
         <ul>
-            {rows.map((row) => {
+            {courses.map((row) => {
                 return (
                     <li key={row.id} className="grid grid-cols-4 gap-4 mb-3">
                         <p className="col-span-2 font-bold">{row.title}</p>
