e0080c931d258e671282835201bb8a8230984c1f
0badran <badran7299@yahoo.com>
2025-05-16 10:00:35 +0300

    Fix database shcema

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -3,7 +3,9 @@ import { Todo } from "@/lib/types";
 import { sql } from "@vercel/postgres";
 
 export default async function TodoUpcoming() {
-    const { rows } = await sql<Todo>`SELECT id, title FROM todos WHERE type = 'upcoming' ORDER BY created_at DESC;`;
+    const { rows } = await sql<Todo>`SELECT id, title FROM todos WHERE type = 'upcoming' ORDER BY createdAt DESC;`;
+    console.log({ rows });
+
     return (
         <DisplayUpcomingTodos data={rows} />
     )
