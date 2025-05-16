6a54c75bae139549e96759e9abad448d17aa23b0
0badran <badran7299@yahoo.com>
2025-05-16 10:03:34 +0300

    Fix database shcema

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,10 +1,10 @@
 import DisplayUpcomingTodos from "@/components/display-upcoming-todos";
 import { Todo } from "@/lib/types";
+import { isLoggedIn } from "@/lib/utils";
 import { sql } from "@vercel/postgres";
 
 export default async function TodoUpcoming() {
-    const { rows } = await sql<Todo>`SELECT id, title FROM todos WHERE type = 'upcoming' ORDER BY createdAt DESC;`;
-    console.log({ rows });
+    const { rows } = isLoggedIn ? await sql<Todo>`SELECT id, title FROM todos WHERE type = 'upcoming' ORDER BY createdAt DESC;` : { rows: [] };
 
     return (
         <DisplayUpcomingTodos data={rows} />
